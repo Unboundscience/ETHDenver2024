ec37e5b4eb47c59ede1265f92d1eb0dfa6b1744a
Father Abraham <abembecker@gmail.com>
2024-02-27 16:46:19 -0700

    chore: added login function and comment login flow.

--- angular-app/src/app/features/landing-pg/landing-pg.component.ts
+++ angular-app/src/app/features/landing-pg/landing-pg.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
+import {Web3Service} from "../../core/services/web3.service";
 
 @Component({
   selector: 'app-landing-pg',
@@ -13,5 +14,36 @@ import {MatIconModule} from "@angular/material/icon";
   styleUrl: './landing-pg.component.scss'
 })
 export class LandingPgComponent {
+    // isConnected: boolean;
+    // isConnecting: boolean;
+    // isDisconnected: boolean;
+    // isReconnecting: boolean;
+    // status: string;
+    constructor(private web3Service: Web3Service) {}
+
+    login(): void {
+        const nextStep = this.getLoginNextStep();
+        alert(nextStep);
+    }
+
+    isWalletConnected(): boolean {
+        const myAcc: any = this.web3Service.getAccountOnce();
+        if (myAcc?.isConnected) {
+            return true;
+        }
+        return false;
+    }
+
+    getLoginNextStep(): 'check-nft' | 'sign-up' {
+        if (this.isWalletConnected()) {
+            return 'check-nft';
+            //check for nft in the wallet
+            //if no nft, go to the scentist / enthusiast
+            // lukso form takes nft meta
+        } else {
+            //navigate to sign up flow
+            return 'sign-up';
+        }
+    }
 
 }
