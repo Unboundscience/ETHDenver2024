36de06561ed69d15c58a22f25967f9f0002e5022
Yoyo <52893812+exanova-y@users.noreply.github.com>
2024-03-01 11:57:05 -0700

    located text on sample microgrants website

--- ai/criteria.py
+++ ai/criteria.py
@@ -1,8 +1,44 @@
-# proposal criteria:
-# activity, impact (scale, neglectedness, solvability)
+# This script evaluates proposals with this criteria:
+# activity, impact (problem scale, neglectedness, proposal solvability)
 
-sample_comment = '''Hi Stephen, thanks for submitting this project! Bio funding is very much outside my personal area of expertise, but I'll ask around and see if anyone in the field might be willing to lend their eyes on reviewing this.
+import requests # the requests library can retrieve the content of the microgrants website page
+from bs4 import BeautifulSoup # BeautifulSoup can parse the fetched HTML content and extract the information we need
+import re # regular expression for filtering text
 
-To set expectations: we haven't issued regrantor budgets for 2024 yet, as we're still fundraising ourselves. It's a shame that this proposal missed the recent ACX Grants round, as it would have been a great fit - but with the upcoming ACX Grants Impact Certs side, there may be an influx of bio-curious investors/donors interested in this.
+# Step 1: get sample text for analysis
+# webscraping from manifund, a microgrants website
 
-Also, I really enjoyed the bits of humor in your proposal - as someone who's fallen backwards into reading lots of these things, it's so nice when a proposal is a delight to read on its own.'''
+# URL of the webpage being scraped
+url = 'https://manifund.org/projects/investigation-of-legionella-as-a-potential-cause-of-type-1-diabetes'
+proposal_main_text_class = '''ProseMirror px-3 text-sm prose leading-relaxed max-w-full prose-a:text-orange-600 prose-a:no-underline text-md prose-p:my-2 prose-ul:my-2 prose-ol:my-2 prose-li:my-2 [&>p]:prose-li:my-0 text-gray-900 prose-blockquote:text-gray-600 prose-a:font-light prose-blockquote:font-light font-light break-anywhere empty:prose-p:after:content-[&quot;\00a0&quot;]'''
+
+# Send a GET request to the webpage
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    print('request successful')
+
+    # Parse the manifund page with BeautifulSoup
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # proposal main text
+    all_paragraphs = soup.find_all('p')
+    print(type(all_paragraphs))
+    print('text located')
+
+    # this loop is not working yet trying to figure it out 
+    for paragraph in all_paragraphs:
+        print(paragraph.type())
+        print(paragraph.get_text())
+        print('---')
+else:
+    print(f"Failed to retrieve the webpage: {response.status_code}")
+
+
+
+# step 2: apply question answering models
+# summarization models
+    
+
+# to do:
+# change find to find_all to accommodate more text
