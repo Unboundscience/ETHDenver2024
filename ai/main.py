276c0ac7f24ee352d30242b042edb3fcd6b69e78
Yoyo <52893812+exanova-y@users.noreply.github.com>
2024-03-02 09:11:15 -0700

    updated prompting and added requirements

--- ai/main.py
+++ ai/main.py
@@ -1,6 +1,7 @@
 from scrape_proposal import get_webpage_proposal
 from analyze_proposal import evaluate
 
-target_url = '''https://manifund.org/projects/investigation-of-legionella-as-a-potential-cause-of-type-1-diabetes'''
+target_url = 'https://manifund.org/projects/investigation-of-legionella-as-a-potential-cause-of-type-1-diabetes' # a sample proposal from manifund
 llm_inputs = get_webpage_proposal(target_url)
 ratings = evaluate(llm_inputs)
+print(ratings)
